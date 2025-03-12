ac6f3147ecc3dac05da28381d73883e4e17c068b
SebastianDevps <guerrasebastian16@gmail.com>
2025-03-11 22:49:16 -0500

    feat: Agregue alias para importaciones

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import path from 'path'
-import tailwindcss from "@tailwindcss/vite"
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,8 +9,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
       "@components": path.resolve(__dirname, "./src/components"),
-
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@store": path.resolve(__dirname, "./src/store"),
     },
   },
-})
+});
